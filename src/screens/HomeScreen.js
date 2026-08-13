f1c34c0bfab9754f2999f49812eaf7fa3e875ba5
redearthbluesky <ian@isalt.digital>
2026-08-13 19:15:42 +0100

    End day backup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,30 +4,30 @@ import styles from "../../styles/styles"
 
 // navigation object provided automatically to screen components defined within a navigator
 const HomeScreen = ({ navigation }) => {
-    // const { navigation } = props;
+    const { navigate } = navigation;
     return (
       <View style={styles.homeContainer}>
         <Text style={styles.H1_b}>HomeScreen</Text>
         <View style={styles.button_container}>
             <Button
-                title="Go to Componets Demo"
-                onPress={() => navigation.navigate('Components', { name: 'Button', company: 'Simple' })}
+                title="Button - Componets"
+                onPress={() => navigate('Components', { name: 'Button', company: 'Simple' })}
             />
             <Button
-                title="Go to Flat List Example"
-                onPress={() => navigation.navigate('FlatList', { name: 'Lists', type: 'Flat List Component' })}
+                title="Button - Flat List"
+                onPress={() => navigate('FlatList', { name: 'Lists', type: 'Flat List Component' })}
             />
             <TouchableOpacity
-                onPress={() => navigation.navigate('Components', { name: 'Touchable', company: 'Opacity' })}
+                onPress={() => navigate('Components', { name: 'Touchable', company: 'Opacity' })}
                 style={styles.TO_button}
             >
-                <Text style={styles.TO_buttonText}>Go Components Touchable Opacity</Text>
+                <Text style={styles.TO_buttonText}>TouchableOpacity - Components</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={() => navigation.navigate('FlatList', { name: 'Touchable', type: 'Opacity' })}
+                onPress={() => navigate('FlatList', { name: 'Touchable', type: 'Opacity' })}
                 style={styles.TO_button}
             >
-                <Text style={styles.TO_buttonText}>Flat List Touchable Opacity</Text>
+                <Text style={styles.TO_buttonText}>TouchableOpacity - FlatList</Text>
             </TouchableOpacity>
         </View>
       </View>
